da75f0878e5786c8fd01361079aa288b1d82183f
Zulfiqar Ali <157399741+Zulfiqar-Ali10@users.noreply.github.com>
2025-12-11 15:59:11 +0500

    FASTAPI

--- FastApi_Postgress/src/models/product_model.py
+++ FastApi_Postgress/src/models/product_model.py
@@ -7,5 +7,4 @@ class Product(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
     price = Column(Float, nullable=False)
-    average = Column(Float, nullable=False)
     in_stock = Column(Boolean, default=True)
